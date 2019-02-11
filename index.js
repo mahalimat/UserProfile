8e540e4a77ce5f2a5158e44a56a3a8a8abd227fa
Mathan <mathanm@gmail.com>
2019-02-11 14:54:28 -0500

    modified client build

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Main starting point of the application
 const express = require("express");
-const http = require("http");
+//const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const app = express();
 const router = require("./router");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -12,10 +11,12 @@ const keys = require("./config/keys");
 // DB Setup
 mongoose.connect(keys.mongoURI);
 
+const app = express();
+
 // App Setup
 app.use(morgan("combined"));
 app.use(cors());
-app.use(bodyParser.json({ type: "*/*" }));
+app.use(bodyParser.json());
 router(app);
 
 if (process.env.NODE_ENV === "production") {
@@ -33,6 +34,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Server Setup
 const port = process.env.PORT || 3090;
-const server = http.createServer(app);
-server.listen(port);
+//const server = http.createServer(app);
+app.listen(port);
 console.log("Server listening on:", port);
